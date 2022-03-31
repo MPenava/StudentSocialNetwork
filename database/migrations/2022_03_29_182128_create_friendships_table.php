d93aa6c1188ee75befb0f84c56d6456d8b3affbd
MPenava <marko.penava@fpmoz.sum.ba>
2022-03-31 09:51:38 +0200

    Initial commit

---
+++ database/migrations/2022_03_29_182128_create_friendships_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('friendships', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('source_id');
+            $table->unsignedBigInteger('target_id');
+            $table->timestamps();
+
+            $table->foreign('source_id')->references('id')->on('users')->onDelete('restrict');
+            $table->foreign('target_id')->references('id')->on('users')->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('friendships');
+    }
+};
